8821e49628f281858bd9889f23c13b1f621a6aed
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2020-10-29 20:52:03 +0200

    more files to make the code readable

---
+++ ScholarChatApp/lib/Screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:ScholarChatApp/Screens/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyHomePage();
+  }
+}
---
+++ ScholarChatApp/lib/models/User.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class User {
+  final String name;
+  final String uid;
+  final String email;
+  final String password;
+
+  User({
+    this.email,
+    this.uid,
+    this.name,
+    this.password,
+  });
+}
